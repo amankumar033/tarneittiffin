b58cd1ecf57f9ea14bbf9f9035e41097ef70818f
amankumar033 <amankumar51462@gmail.com>
2025-09-12 20:21:57 +0530

    updated

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 3011),
-  url: env('PUBLIC_URL', 'http://localhost:3011'),
+  url: env('PUBLIC_URL', 'https://admin.tarneittiffin.com'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
